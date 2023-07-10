218fe08b512bd71e31901f1e17ddb4fec8e0a1b0
kimheeyoungkim <137054517+kimheeyoungkim@users.noreply.github.com>
2023-07-10 17:00:05 +0900

    Add files via upload

---
+++ script/popup1.js
@@ -0,0 +1,54 @@
+$(function () {
+    $('.edtn1').click(function () {
+        $('#pb_popup').css({'display':'flex'});// .show()<-확 보임
+    });
+    $('#pb_popup p a').click(function () {
+        $('#pb_popup').fadeOut()// .hide()<-확 없어짐
+    });
+
+});
+
+$(function () {
+    $('.con_a').click(function () {
+        $('#journal_popup').css({'display':'flex'});// .show()<-확 보임
+    });
+    $('#journal_popup p a').click(function () {
+        $('#journal_popup').fadeOut()// .hide()<-확 없어짐
+    });
+
+});
+
+$(function () {
+    $('.event_a_1').click(function () {
+        $('#event_popup_1').css({'display':'flex'});// .show()<-확 보임
+    });
+    $('#event_popup_1 p a').click(function () {
+        $('#event_popup_1').fadeOut()// .hide()<-확 없어짐
+    });
+
+});
+
+
+$(function () {
+    $('.event_a_2').click(function () {
+        $('#event_popup_2').css({'display':'flex'});// .show()<-확 보임
+    });
+    $('#event_popup_2 p a').click(function () {
+        $('#event_popup_2').fadeOut()// .hide()<-확 없어짐
+    });
+
+});
+
+$(function () {
+    $('.notice_a_8').click(function () {
+        $('#notice_popup').css({'display':'flex'});// .show()<-확 보임
+    });
+    $('#notice_popup p a').click(function () {
+        $('#notice_popup').fadeOut()// .hide()<-확 없어짐
+    });
+
+});
+
+
+
+
